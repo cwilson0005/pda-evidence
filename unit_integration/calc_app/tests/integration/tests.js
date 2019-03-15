f7531ed8dea5ec8d93877e342f27c0cbde20bb2f
Conor <cwilson0005@hotmail.co.uk>
2019-03-15 02:43:29 +0000

    finished

--- unit_integration/calc_app/tests/integration/tests.js
+++ unit_integration/calc_app/tests/integration/tests.js
@@ -16,4 +16,54 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('the number buttons should update the display of the running total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('23')
+  })
+
+  it('the arithmetical operations should update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
+  it('should be able to chain multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
+  it('should be able to output based on a range of different values', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-63.333333333333336')
+  })
+
+  it('should be able to output "Not a number" if an attempt to divide by 0 is made', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal("Infinity")
+  })
+
 });
